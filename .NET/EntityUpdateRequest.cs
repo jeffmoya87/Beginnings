296791301a4eadea85ef2cfb99c2b11cb9cd1222
jeffmoya87 <97494340+jeffmoya87@users.noreply.github.com>
2022-05-13 15:53:14 -0700

    Update EntityUpdateRequest.cs

--- .NET/EntityUpdateRequest.cs
+++ .NET/EntityUpdateRequest.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
 
-namespace Sabio.Models.Requests
+namespace System.Models.Requests
 {
     public class EntityUpdateRequest : IModelIdentifier
     {
