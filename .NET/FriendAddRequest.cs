7c37cfaffa80f163e08716772c3d67fef2a908e8
jeffmoya87 <97494340+jeffmoya87@users.noreply.github.com>
2022-05-13 15:53:47 -0700

    Update FriendAddRequest.cs

--- .NET/FriendAddRequest.cs
+++ .NET/FriendAddRequest.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sabio.Models.Requests.Friends
+namespace System.Models.Requests.Friends
 {
     public class FriendAddRequest
     {
