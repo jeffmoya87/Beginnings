dd0c710c1ce865a343730d4537f8675ecf1af794
jeffmoya87 <97494340+jeffmoya87@users.noreply.github.com>
2022-05-12 17:11:00 -0700

    Create BaseAddress.cs

---
+++ .NET/BaseAddress.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Domain
+{
+    public class BaseAddress
+    {
+        public int Id { get; set; }
+        public string LineOne { get; set; }
+        public int SuiteNumber { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
