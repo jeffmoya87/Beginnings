dcca8937520611f1f35d20353c1e2d3e63f1b45e
jeffmoya87 <97494340+jeffmoya87@users.noreply.github.com>
2022-05-13 16:04:43 -0700

    Update UserUpdateRequest.cs

--- .NET/UserUpdateRequest.cs
+++ .NET/UserUpdateRequest.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sabio.Models.Requests.Users
+namespace System.Models.Requests.Users
 {
     public class UserUpdateRequest: UserAddRequest
     {
